e331c299efde50ef3d0a7a8b578f3dae2cea4676
greengod540 <drgamesbg@gmail.com>
2025-09-18 11:00:38 +0300

    braaaaaat

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+float diffToKelvin = -273.15f;
+Console.WriteLine("Type Temp inputted: ");
+string typeTemp = Console.ReadLine();
+typeTemp = typeTemp.ToLower();
+
+if (typeTemp == "celcius")
+{
+    Console.WriteLine("Input: ");
+    float t = float.Parse(Console.ReadLine());
+    Console.WriteLine(t - diffToKelvin);
+}else if (typeTemp == "kelvin")
+{
+    Console.WriteLine("Input: ");
+    float t = float.Parse(Console.ReadLine());
+    Console.WriteLine(t + diffToKelvin);
+}
+
+
