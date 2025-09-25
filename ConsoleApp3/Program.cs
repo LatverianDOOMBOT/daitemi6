3849f3e57de815f6fc6f250ab5d0e4df00f0ff7c
LatverianDOOMBOT <drgamesbg@gmail.com>
2025-09-25 10:38:39 +0300

    all q's answered

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-float diffToKelvin = -273.15f;
+float diffToKelvin = -273.15f; // 1. vupros - dobavq se watch nz dali se e promenila ne sum proveril
 Console.WriteLine("Type Temp inputted: ");
 string typeTemp = Console.ReadLine();
 typeTemp = typeTemp.ToLower();
@@ -8,7 +8,7 @@ typeTemp = typeTemp.ToLower();
 
 if (!string.IsNullOrEmpty(typeTemp)) // ne mi se pishat 10000 takiva stiga tolkova nqma da proverqvam float t
 {
-    float t = float.Parse(Console.ReadLine());
+    float t = float.Parse(Console.ReadLine()); // samo edin put se chete conzolata
     if (typeTemp == "celcius")
     {
         Console.WriteLine("Input: ");
@@ -21,7 +21,7 @@ if (!string.IsNullOrEmpty(typeTemp)) // ne mi se pishat 10000 takiva stiga tolko
 
         t = t + diffToKelvin;
     }
-    Console.WriteLine($"Converted Temp: {t}");
+    Console.WriteLine($"Converted Temp: {t}"); //ppc ne sum se setil za teq raboti shoto programite gi pravq za 5 min na kraq na chasa
 }
 
 
--- ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89bed01396721698a50a5a0c9b18aa4395b035dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8c228921d88d6d74fcad6d2d15031f79fedbba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
